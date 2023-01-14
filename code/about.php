86ad80067dd3f6e9d9ba5a0428a6bf0ceb2372a3
2deadmen <kartikhegde.2002@gmail.com>
2023-01-14 13:29:41 +0530

    styling

--- code/about.php
+++ code/about.php
@@ -1,78 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
 <head>
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>About us</title>
-  <link rel="stylesheet" href="{aboutus.css"/>
-  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
-    integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
-  <link rel="icon" href="{% static 'PICO-LOGO-SHORT.png' %}" type="image/gif">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="about.css">
+    <link rel="stylesheet" href="styl.css">
+  
+   
+  
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <title>Homecleaners</title>
 </head>
 
 <body>
 <?php
-require('session.php');
-?>
-  <!-- -----------------------------------------------company---------------------------------------------------------------- -->
-  <div class="company">
-    <div class="img">
-      <img src="" alt="" />
-    </div>
-    <div class="company-info">
-      <span>Online<span class="our">Home services</span></span>
-      <p>
+   require('session.php'); 
+    ?><nav>
+         <label class="logo">Home care Services</label>
+         <ul>
+
+         <li><a href="index.php">Home</a></li>
+
+<li><a href="about.php">about</a></li>
+
+            <li>
+               <a href="#">Services
+               <i class="fas fa-caret-down"></i>
+               </a>
+               <ul>
+                  <li><a href="Painters.php">Painters</a></li>
+                  <li><a href="Electrician.php">Electrician</a></li>
+                  <li><a href="Cook.php">Cook</a></li>               
+                 <li><a href="Homecleaners.php">Homecleaners</a></li>
+                 </ul>
+                 </li>
+                 <li><a href='<?php
+                  if(isset($_SESSION['worker']))
+                 {echo "services.php"; }
+                 else{
+                   echo "cust_services.php";} ?>'>Request status</a></li>
+                 <li><a href="logout.php">Logout</a></li>
+                 <!-- <li><a href="login.php">login</a></li>
+                 <li><a href="register.php">registration</a></li> -->
+               <!-- <li><a href="contact.php">Contact</a></li> -->
+               </ul>
+      </nav>
+  <!-- -----------------------------------------------company------------------------------------------------------------------>
+
+   
+<h1 align='center' class='my-4'>  <b>Online Home Services</b></h1>
+<div class="company-info container headcont">    
+<div class="container content para">
         <b>Online Home services</b> Now day's web services technology is widely used to integrate heteroginious system and develope new applications.
-        <br>and here is a website of online booking for servents for domestic house chores and to hire workers so we made our website
-    <br> the main objective of this website is to provide the better and easy life for customers to get quick solutions for searching thier day to day services.  </p>
-    </div>
-  </div>
+       and here is a website of online booking for servents for domestic house chores and to hire workers so we made our website
+    the main objective of this website is to provide the better and easy life for customers to get quick solutions for searching thier day to day services. </div>
+    <div class="container content"><img width='560' height='350' src="homeimage.jpg" alt="" /></div>
+     </div>
   <!-- ---------------------------------------------------------------------------------------------------------------------- -->
   <!-- ----------------------------------------------------team-------------------------------------------------------------- -->
-  <div class="team"><span></span></div>
-  <div class="container">
-    <div class="card">
-      <div class="card-image loading"><img src="homeimage.jpg" alt="" /></div>
-      <div class="card-info">
-        <h3 class="card-title loading"><span>Our Services are<span class="yellow-surname"></span></span></h3>
-        <p class="card-description loading">
-          <span class="personal-info">
-            <span class="info">Painters</span> <br>
-            <div class="card-image loading"><img src="painters.jpg" alt="" /></div>
-           </span>
-        </p>         
-         <span class="personal-info">
-            <span class="info">Electricians</span> <br>
-            <div class="card-image loading"><img src="electrition.jpg" alt="" /></div>
-           </span>
-           <span class="personal-info">
-            <span class="info">Cook</span> <br>
-            <div class="card-image loading"><img src="cook.jpg" alt="" /></div>
-           </span>
-           <span class="personal-info">
-            <span class="info">Home cleaners</span> <br>
-            <div class="card-image loading"><img src="homecc.jpg" alt="" /></div>
-           </span>
+  <div class='container'>
+   <h1 align='center' class='my-4'>   <b>Our Services</b> 
+</h1>
+    <div class='imgcont'>
+    <div  class='mx-2 my-4 img img1' ><br><span class='text'>PAINTER</span></div>
+    <div class='mx-2 my-4 img img2'  ><br><span class='text'>ELECTRICIAN</span></div>
+    </div>
+    <div class='imgcont'>
+    <div class='mx-2 my-4 img img3' ><br><span class='text'>COOK</span></div>
+    <div class='mx-2  my-4 imgs img img4'><br><span class='text texts'>HOMECLEANER</span></div>
+    </div>
+  </div>
+  <div class='container' align='center'>      
         <br>the website provides the user with the flexibility of accessing the services and facilities.
         <br>the user can able to surf from their comfort place and can book a slot for services and they 
         <br>can provide the timing also even they can order services for their freinds and family members
         <br>if they are not able to get service providers.....
       </div>
-    </div>
     
-
-  </div>
   <!-- ----------------------------------------footer------------------------------------------ -->
-  <footer></footer>
+  <footer class='container my-4' align='center'>
 
-        </ul>
+      
         <ul class="box">
           <li class="link_name">For More information Contact us</li>
           <li><a target="on_blank" href="tel:+919741132768">+91 9741132768</a></li>
           <li><a target="on_blank" href="mailto:'sandhyasa931@gmail.com'">sandhyasa931@gmail.com</a></li>
         </ul>
+        <h1 class="center ">THANK YOU</h1>
+        </footer>
     </body>
-</div>
 
-THANK YOU
+
+
 
 </html>
